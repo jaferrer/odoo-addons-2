388e33740464c7020ee7001614cd721605b0fa60
Nicolas Piganeau <npi@m4x.org>
2015-02-20 17:51:00 +0100

    Started work on barcode UI

---
+++ stock_product_warning/static/src/js/stock_product_warning.js
@@ -0,0 +1,16 @@
+openerp.stock_split_picking = function(instance){
+
+//    instance.stock.PickingEditorWidget.include({
+//
+//        get_rows: function(){
+//            var model = this.getParent();
+//            this.rows = this._super();
+//            var self = this;
+//            _.each( this.rows.cols, function(col){
+//                col.push(procurement_warning: )
+//            }
+//
+//        },
+
+
+}
