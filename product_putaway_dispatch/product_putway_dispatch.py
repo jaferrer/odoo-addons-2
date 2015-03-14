56769d5ae7b9406faa669d623ffffb938032ead8
Nicolas Piganeau <npi@m4x.org>
2015-03-14 14:56:22 +0100

    [IMP] product_putaway_dispatch: Packs are now dispatched If they contain only one product and if it is possible. Other packs are unpacked

--- product_putaway_dispatch/product_putway_dispatch.py
+++ product_putaway_dispatch/product_putway_dispatch.py
@@ -45,38 +45,88 @@ class product_putaway_dispatch_transfer_details(models.TransientModel):
     def action_dispatch(self):
         for transfer in self:
             moves = {}
-            products = []
-            # Prepare unpacking of all packs
-            transfer.packop_ids.prepare_unpack()
-            # Get the products list
+            qty_to_dispatch = {}
+            # Get the quantity to dispatch for each product
+            for op in transfer.packop_ids:
+                # First in packs
+                quants = self.env['stock.quant'].browse(op.package_id.get_content())
+                if not all([(q.product_id == quants[0].product_id) for q in quants]):
+                    # If the pack is not composed of a single product, we prepare unpacking to handle the quants as
+                    # bulk products
+                    op.prepare_unpack()
+                    continue
+                for quant in quants:
+                    if quant.product_id in qty_to_dispatch:
+                        qty_to_dispatch[quant.product_id] += quant.qty
+                    else:
+                        qty_to_dispatch[quant.product_id] = quant.qty
             for op in transfer.item_ids:
-                if not op.product_id in products:
-                    products.append(op.product_id)
-            # Get the outgoing moves of all child locations (needs) for each product
-            for product_id in products:
-                if not product_id in moves:
-                    moves[product_id] = self.env['stock.move'].search(
+                # Then in bulk products
+                if op.product_id in qty_to_dispatch:
+                    qty_to_dispatch[op.product_id] += op.quantity
+                else:
+                    qty_to_dispatch[op.product_id] = op.quantity
+
+            # Iterate on each product
+            for product_id, qty_todo in qty_to_dispatch.iteritems():
+                need_moves = self.env['stock.move'].search(
                                                 [('location_id','child_of',transfer.picking_destination_location_id.id),
                                                  ('product_id','=',product_id.id),('state','=','confirmed')],
                                                 order="priority DESC, date")
-            # Iterate on each product
-            for product_id, move_list in moves.iteritems():
+                location_qty = {}
+                qty_left = qty_todo
+                # Get the quantity to dispatch for each location and set it in location_qty dict
+                for move in need_moves:
+                    if qty_left <= 0:
+                        break
+                    qty_to_add = min(move.product_qty, qty_left)
+                    if move.location_id in location_qty:
+                        location_qty[move.location_id] += qty_to_add
+                    else:
+                        location_qty[move.location_id] = qty_to_add
+                    qty_left -= qty_to_add
+
+                # First try to dispatch entire packs
+                # We only have packs with a single product since we prepared unpacking for the others
+                remaining_packops = transfer.packop_ids.filtered(lambda x:
+                                self.env['stock.quant'].browse(x.package_id.get_content())[0].product_id == product_id)
+                remaining_packops = remaining_packops.sorted(key=lambda x:sum([q.qty for q in
+                                                        self.env['stock.quant'].browse(x.package_id.get_content())]),
+                                                             reverse=True)
+                for op in remaining_packops:
+                    # We try to find a location where we need at least the whole qty of the pack
+                    for location, qty in location_qty.iteritems():
+                        if qty <= 0:
+                            continue
+                        pack_qty = sum([q.qty for q in self.env['stock.quant'].browse(op.package_id.get_content())])
+                        if pack_qty < qty:
+                            # We found a location, so we dispatch the pack to location and go for the next pack
+                            op.destinationloc_id = location
+                            location_qty[location] -= pack_qty
+                            remaining_packops = remaining_packops - op
+                            break
+
+                # We prepare unpacking for the remaining packs to handle them as bulk products
+                remaining_packops.prepare_unpack()
+                # Then we fetch the bulk product operation lines
                 op_items = transfer.item_ids.search([('product_id','=',product_id.id),('transfer_id','=',transfer.id)])
-                # Iterate on each operation to keep packages, lots and owners intact
+                # Iterate on each bulk product operations to dispatch them
                 for op in op_items:
                     # We get the quantity to dispatch and set the quantity of the operation to 0
                     qty_todo = op.quantity
                     op.quantity = 0
                     # We initialize a recordset holding all the lines split from op
                     split_ops = op
-                    for move in move_list:
+
+                    for location, qty_loc in location_qty.iteritems():
                         if qty_todo <= 0:
                             break
-                        dest_id = move.location_id
-                        qty = min(qty_todo, move.product_qty)
+                        if qty_loc <= 0:
+                            continue
+                        qty = min(qty_todo, qty_loc)
                         # Check if we have already a line with the wanted destination location
                         for target_op in split_ops:
-                            if target_op.destinationloc_id == dest_id:
+                            if target_op.destinationloc_id == location:
                                 # If we do, we add the quantity to dispatch to this line
                                 target_op.quantity += qty
                                 break
@@ -87,10 +137,11 @@ class product_putaway_dispatch_transfer_details(models.TransientModel):
                             new_op.write({
                                 'quantity': qty,
                                 'packop_id': False,
-                                'destinationloc_id': dest_id.id,
+                                'destinationloc_id': location.id,
                                 'result_package_id': False,
                             })
                             split_ops = split_ops | new_op
+                        location_qty[location] -= qty
                         qty_todo -= qty
                     # We delete op if it has not been allocated some quantity in between
                     if op.quantity == 0:
