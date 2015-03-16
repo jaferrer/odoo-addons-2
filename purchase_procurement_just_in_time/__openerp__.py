e0830e56a5771fb4c37e7ffeb559e6002f3dcc84
Nicolas Piganeau <npi@m4x.org>
2015-03-16 09:00:20 +0100

    Added module purchase_procurement_just_in_time First version

---
+++ purchase_procurement_just_in_time/__openerp__.py
@@ -0,0 +1,66 @@
+# -*- coding: utf8 -*-
+#
+# Copyright (C) 2014 NDP Systèmes (<http://www.ndp-systemes.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+{
+    'name': 'Purchase Just-In-Time Extension',
+    'version': '0.1',
+    'author': 'NDP Systèmes',
+    'maintainer': 'NDP Systèmes',
+    'category': 'Purchase',
+    'depends': ['purchase','purchase_planning_improved','purchase_group_by_period','stock_procurement_just_in_time'],
+    'description': """
+Purchase Just-In-Time Extension
+===============================
+Just-In-Time (JIT) is an inventory strategy companies employ to increase efficiency and decrease waste by receiving
+goods only as they are needed in the production process, thereby reducing inventory costs.
+
+When JIT procurement is enabled through the stock_procurement_just_in_time module, draft POs are created by the
+scheduler with its required date set just as needed. While this is good to reduce inventory to the minimum, it requires
+purchasing officers to be particularly responsive to changes of schedule. This implies being able to contact suppliers
+to modify scheduling of already placed orders and to register the new promised date.
+
+This modules implements a supplier backlog which enable purchase officers to see all the purchase order lines at
+once, whichever purchase order they come from. The supplier backlog provides the following operational messages on
+purchase order lines:
+
+- "LATE by n days": The order line planned date is behind the required date by n days. This message appears only when n
+  is above a configurable preset value.
+- "EARLY by n days": The order line planned date is ahead of the required date by n days. This message appears only when
+  n is above a configurable preset value.
+- "REDUCE QTY to n [UoM]": One of the procurement order which generated the purchase order line has been cancelled. The
+  qty ordered in this line is to be reduced to the quantity given.
+
+Notes
+-----
+- This module depends on purchase_planning_improved and as such on purchase_working_days, which means that all the
+  scheduling is made based on working days.
+
+""",
+    'website': 'http://www.ndp-systemes.fr',
+    'data': [
+        'views/res_config_view.xml',
+        'views/purchase_procurement_jit.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+    'application': False,
+}
---
+++ purchase_procurement_just_in_time/models/res_config.py
@@ -0,0 +1,62 @@
+# -*- coding: utf8 -*-
+#
+# Copyright (C) 2014 NDP Systèmes (<http://www.ndp-systemes.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from openerp import fields, models, api
+
+
+class purchase_jit_config(models.TransientModel):
+    _inherit = 'purchase.config.settings'
+
+    opmsg_min_late_delay = fields.Integer("Delay to be late (in days)",
+                                          help="Minimum delay to create an operational message specifying that the "
+                                               "purchase order line is late. If the planned date is less than this "
+                                               "number of days beyond the required date, no message will be displayed."
+                                               "\nDefaults to 0 days.")
+    opmsg_min_early_delay = fields.Integer("Delay to be early (in days)",
+                                          help="Minimum delay to create an operational message specifying that the "
+                                               "purchase order line is early. If the planned date is less than this "
+                                               "number of days beyond the required date, no message will be displayed."
+                                               "\nDefaults to 7 days.")
+
+    @api.multi
+    def get_default_opmsg_min_late_delay(self):
+        opmsg_min_late_delay = self.env['ir.config_parameter'].get_param(
+                                                   "purchase_procurement_just_in_time.opmsg_min_late_delay", default=0)
+        return {'opmsg_min_late_delay': int(opmsg_min_late_delay)}
+
+    @api.multi
+    def get_default_opmsg_min_early_delay(self):
+        opmsg_min_early_delay = self.env['ir.config_parameter'].get_param(
+                                                   "purchase_procurement_just_in_time.opmsg_min_early_delay", default=7)
+        return {'opmsg_min_early_delay': int(opmsg_min_early_delay)}
+
+    @api.multi
+    def set_opmsg_min_late_delay(self):
+        config_parameters = self.env["ir.config_parameter"]
+        for record in self:
+            config_parameters.set_param("purchase_procurement_just_in_time.opmsg_min_late_delay",
+                                        record.opmsg_min_late_delay or 0)
+
+    @api.multi
+    def set_opmsg_min_early_delay(self):
+        config_parameters = self.env["ir.config_parameter"]
+        for record in self:
+            config_parameters.set_param("purchase_procurement_just_in_time.opmsg_min_early_delay",
+                                        record.opmsg_min_early_delay or 7)
+
