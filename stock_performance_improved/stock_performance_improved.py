d35304e1f3f8d1cb3e504544a53e1d2d3750afe0
Nicolas Piganeau <npi@m4x.org>
2015-04-02 09:57:17 +0200

    Revert "[IMP] stock_performance_improved: added filter on do_transfer"
    This reverts commit b0525e4e32693d6a6b56cb057a621e37505e1a74.

--- stock_performance_improved/stock_performance_improved.py
+++ stock_performance_improved/stock_performance_improved.py
@@ -18,7 +18,7 @@
 #
 import time
 
-from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT, float_compare
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from openerp import fields, models, api, exceptions, _
 
 class stock_picking_performance_improved(models.Model):
@@ -95,57 +95,5 @@ class stock_picking_performance_improved(models.Model):
             picking.message_post(body=_("New picking <em>%s</em> <b>created</b>.") % (done_picking.name))
             picking.write({'backorder_id': done_picking.id})
             return done_picking
-        return self.env['stock.picking']
-
-    @api.cr_uid_ids_context
-    def do_transfer(self, cr, uid, picking_ids, context=None):
-        """
-            If no pack operation, we do simple action_done of the picking
-            Otherwise, do the pack operations
-        """
-        if not context:
-            context = {}
-        stock_move_obj = self.pool.get('stock.move')
-        for picking in self.browse(cr, uid, picking_ids, context=context):
-            if not picking.pack_operation_ids:
-                self.action_done(cr, uid, [picking.id], context=context)
-                continue
-            else:
-                need_rereserve, all_op_processed = self.picking_recompute_remaining_quantities(cr, uid, picking, context=context)
-                #create extra moves in the picking (unexpected product moves coming from pack operations)
-                todo_move_ids = []
-                if not all_op_processed:
-                    todo_move_ids += self._create_extra_moves(cr, uid, picking, context=context)
-
-                #split move lines if needed
-                toassign_move_ids = []
-                move_lines = [m for m in picking.move_lines if m.state == 'assigned' or m.availability > 0.0]
-                for move in move_lines:
-                    remaining_qty = move.remaining_qty
-                    if move.state in ('done', 'cancel'):
-                        #ignore stock moves cancelled or already done
-                        continue
-                    elif move.state == 'draft':
-                        toassign_move_ids.append(move.id)
-                    if float_compare(remaining_qty, 0,  precision_rounding = move.product_id.uom_id.rounding) == 0:
-                        if move.state in ('draft', 'assigned', 'confirmed'):
-                            todo_move_ids.append(move.id)
-                    elif float_compare(remaining_qty,0, precision_rounding = move.product_id.uom_id.rounding) > 0 and \
-                                float_compare(remaining_qty, move.product_qty, precision_rounding = move.product_id.uom_id.rounding) < 0:
-                        new_move = stock_move_obj.split(cr, uid, move, remaining_qty, context=context)
-                        todo_move_ids.append(move.id)
-                        #Assign move as it was assigned before
-                        toassign_move_ids.append(new_move)
-                if need_rereserve or not all_op_processed:
-                    if not picking.location_id.usage in ("supplier", "production", "inventory"):
-                        self.rereserve_quants(cr, uid, picking, move_ids=todo_move_ids, context=context)
-                    self.do_recompute_remaining_quantities(cr, uid, [picking.id], context=context)
-                if todo_move_ids and not context.get('do_only_split'):
-                    self.pool.get('stock.move').action_done(cr, uid, todo_move_ids, context=context)
-                elif context.get('do_only_split'):
-                    context = dict(context, split=todo_move_ids)
-            self._create_backorder(cr, uid, picking, context=context)
-            if toassign_move_ids:
-                stock_move_obj.action_assign(cr, uid, toassign_move_ids, context=context)
-        return True
+        return False
 
