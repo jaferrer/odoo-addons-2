48efeb948317a4176398018460fc69a00e14ac6c
thomas <thomas.lemaire@ndp-systemes.fr>
2015-05-21 09:46:44 +0200

    Application des modifications suggérées pour la merge request #7.

--- manufacturing_order_update/manufacturing_order_update.py
+++ manufacturing_order_update/manufacturing_order_update.py
@@ -25,45 +25,31 @@ class mrp_production(models.Model):
 
     @api.one
     def update_moves(self):
-        useless_moves = []
         changes_to_do = []
         list_products_to_change = []
         needed_new_moves = []
+        useless_moves = self.move_lines.filtered(lambda m: m.product_id not in [x.product_id for x in self.product_lines])
+        useless_moves.with_context({'cancel_procurement': True}).action_cancel()
         for item in self.move_lines:
-            if not item.product_id in [x.product_id for x in self.product_lines]:
-                useless_moves += [item]
-        for move in useless_moves:
-            move.with_context({'cancel_procurement': True}).action_cancel()
-        for item in self.move_lines:
-            total_old_need = 0
-            for x in self.move_lines:
-                if x.product_id == item.product_id:
-                    total_old_need += x.product_uom_qty
-            total_new_need = sum([x.product_qty for x in self.product_lines if x.product_id == item.product_id])
-            if not item.product_id in list_products_to_change and total_new_need != total_old_need and total_new_need != 0:
-                changes_to_do += [[item.product_id, total_new_need - total_old_need, total_new_need, total_old_need]]
-                list_products_to_change += [item.product_id]
-        for change in changes_to_do:
-            product = change[0]
-            qty = change[1]
+            if not item.product_id in list_products_to_change:
+                total_old_need = sum([x.product_uom_qty for x in self.move_lines if x.product_id == item.product_id])
+                total_new_need = sum([x.product_qty for x in self.product_lines if x.product_id == item.product_id])
+                if total_new_need != total_old_need and total_new_need != 0:
+                    changes_to_do += [(item.product_id, total_new_need - total_old_need, total_new_need, total_old_need)]
+                    list_products_to_change += [item.product_id]
+        for product, qty, total_new_need, total_old_need in changes_to_do:
             if qty > 0:
                 move = self._make_consume_line_from_data(self, product, product.uom_id.id, qty, False, 0)
                 self.env['stock.move'].browse(move).action_confirm()
             else:
-                _sum = sum([x.product_qty for x in self.move_lines if x.product_id == product])
-                while _sum > change[2]:
-                    for item in self.move_lines:
-                        list_1 = [x.product_qty for x in self.move_lines if x.product_id == product and x.state != 'cancel']
-                        maximum = 0
-                        if len(list_1) != 0:
-                            maximum = max(list_1)
-                        if item.product_id == product and item.product_qty == maximum and item.state != 'cancel':
-                            _sum -= item.product_qty
-                            item.with_context({'cancel_procurement': True}).action_cancel()
-                            break
-                if _sum < change[2]:
-                    product = change[0]
-                    move = self._make_consume_line_from_data(self, product, product.uom_id.id, change[2] - _sum, False, 0)
+                moves = self.move_lines.filtered(lambda m: m.product_id==product).sorted(key=lambda m: m.product_qty, reverse=True)
+                _sum = sum([x.product_qty for x in moves])
+                while _sum > total_new_need:
+                    moves[0].with_context({'cancel_procurement': True}).action_cancel()
+                    _sum -= moves[0].product_qty
+                    moves -= moves[0]
+                if _sum < total_new_need:
+                    move = self._make_consume_line_from_data(self, product, product.uom_id.id, total_new_need - _sum, False, 0)
                     self.env['stock.move'].browse(move).action_confirm()
 
         for item in self.product_lines:
@@ -72,7 +58,7 @@ class mrp_production(models.Model):
 
         for item in needed_new_moves:
             product = item.product_id
-            move = self._make_consume_line_from_data(self, product, product.uom_id.id, item['product_qty'], False, 0)
+            move = self._make_consume_line_from_data(self, product, product.uom_id.id, item.product_qty, False, 0)
             self.env['stock.move'].browse(move).action_confirm()
 
     @api.multi
