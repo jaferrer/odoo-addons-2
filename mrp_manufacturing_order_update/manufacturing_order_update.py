5534a7a631bab677825b3e3859d5ff806da14604
thomas <thomas.lemaire@ndp-systemes.fr>
2015-09-10 09:34:55 +0200

    Commit de test (problème de syntaxe ?)

--- mrp_manufacturing_order_update/manufacturing_order_update.py
+++ mrp_manufacturing_order_update/manufacturing_order_update.py
@@ -67,11 +67,10 @@ class mrp_production(models.Model):
     def write(self, vals):
         result = super(mrp_production, self).write(vals)
         productions_to_update = self
-        if vals.get('product_lines'):
-            for i in range(3):
-                if i in [x[0] for x in vals.get('product_lines')]:
-                    productions_to_update.update_moves()
-                    break
+        if vals.get('product_lines') and ((1 in [x[0] for x in vals.get('product_lines')])
+                                          or (2 in [x[0] for x in vals.get('product_lines')])
+                                          or (0 in [x[0] for x in vals.get('product_lines')])):
+            productions_to_update.update_moves()
         return result
 
     @api.multi
