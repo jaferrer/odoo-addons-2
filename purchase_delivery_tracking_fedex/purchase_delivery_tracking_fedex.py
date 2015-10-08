de7d25d320f044dbf953e561aff9b14ca97bab91
thomas <thomas.lemaire@ndp-systemes.fr>
2015-10-08 14:34:10 +0200

    Retrait d'un import inutile

--- purchase_delivery_tracking_fedex/purchase_delivery_tracking_fedex.py
+++ purchase_delivery_tracking_fedex/purchase_delivery_tracking_fedex.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from openerp import models, fields, api, _
+from openerp import models, api, _
 from urllib2 import urlopen
 from urllib import urlencode
 import json
