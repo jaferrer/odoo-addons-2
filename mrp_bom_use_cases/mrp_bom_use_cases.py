e35569f6c45d01a05d49cfe9d9be4b9321d43be0
thomas <thomas.lemaire@ndp-systemes.fr>
2015-10-15 09:22:32 +0200

    Oubli

--- mrp_bom_use_cases/mrp_bom_use_cases.py
+++ mrp_bom_use_cases/mrp_bom_use_cases.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from openerp import fields, models, api, _
+from openerp import fields, models, api
 
 
 class MrpBomLine(models.Model):
