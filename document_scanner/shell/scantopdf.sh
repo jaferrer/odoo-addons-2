4dd7d7c3e359669049a2436d0afcac55fa78d2f4
asalaun <asalaun@NDP-ASALAUN>
2015-10-16 17:13:43 +0200

    ajout module css + correction scanner

--- document_scanner/shell/scantopdf.sh
+++ document_scanner/shell/scantopdf.sh
@@ -11,12 +11,13 @@ echo "################## Scanning ###################"
 scanimage --device-name=$device -y 279.4 -x 215.9 --batch --format=tiff --mode Color --resolution 300 --source ADF
 echo "############## Converting to PDF ##############"
 #Use tiffcp to combine output tiffs to a single mult-page tiff
-files=$(ls -1 |grep *.tif |wc -l)
+files=$(find . -type f -printf x | wc -c)
 if [ $files -gt 0 ];
 then
 	tiffcp -c lzw out*.tif output.tif ;
 
 #Convert the tiff to PDF
+echo "Convert the tiff to PDF"
 tiff2pdf output.tif > $path/$outname
 fi
 cd ..
