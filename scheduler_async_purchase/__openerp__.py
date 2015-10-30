ae8b69bbd3748745d5745396e9ce74cac6a4f7df
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2015-10-30 16:14:36 +0100

    Added scheduler_async_purchase module

---
+++ scheduler_async_purchase/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf8 -*-
+#
+# Copyright (C) 2014 NDP Systèmes (<http://www.ndp-systemes.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+{
+    'name': 'Asynchronous Scheduler (purchase)',
+    'version': '0.1',
+    'author': 'NDP Systèmes',
+    'maintainer': 'NDP Systèmes',
+    'category': 'Purchase',
+    'depends': ['scheduler_async','purchase'],
+    'description': """
+Asynchronous Scheduler (purchase)
+=================================
+This module adds the necessary data to have scheduler_async work when the purchase module is installed.
+""",
+    'website': 'http://www.ndp-systemes.fr',
+    'data': [
+        'scheduler_async_purchase_data.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': True,
+    'license': 'AGPL-3',
+    'application': False,
+}
