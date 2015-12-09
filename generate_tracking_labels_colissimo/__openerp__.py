962d6b8ad705ada653b1bd1484f2125a43994bd6
thomas <thomas.lemaire@ndp-systemes.fr>
2015-12-09 09:01:45 +0100

    Correction erreur dans droits accès

--- generate_tracking_labels_colissimo/__openerp__.py
+++ generate_tracking_labels_colissimo/__openerp__.py
@@ -30,7 +30,7 @@ Génération des étiquettes de suivi (colissimo)
 Ce module permet de générer des étiquettes d'envoi avec Colissimo.
 """,
     'website': 'http://www.ndp-systemes.fr',
-    'data': ['ir.model.access.csv',
+    'data': ['security/ir.model.access.csv',
              'generate_tracking_labels_colissimo.xml',
              'res_config.xml'],
     'demo': [],
