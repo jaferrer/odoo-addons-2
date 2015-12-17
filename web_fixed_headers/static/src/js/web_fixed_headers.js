c61f16bcf77320a6e57071de4ed2210be47559f3
asalaun <asalaun@NDP-ASALAUN>
2015-12-17 14:42:18 +0100

    odoo-addons/sirail#227
     correction cross navigation
     suppresion entete list vide
     ajout du tri
     alignement colonne

--- web_fixed_headers/static/src/js/web_fixed_headers.js
+++ web_fixed_headers/static/src/js/web_fixed_headers.js
@@ -12,34 +12,36 @@ openerp.web_fixed_headers = function(instance) {
 		init: function (group, opts) {
 			this._super(group, opts);
 			this.$table=$('.oe_view_manager_view_list table.oe_list_content:first');
-			this.$table.addClass('fixed_headers');
 		},
 		
 		resize_list_content :function () {
 			var self=this;
-			
 			if(this.$table.find('tbody tr:first').children().size()>0) {
-			
-				if($('.oe_view_manager_view_list table.oe_list_header_custom:first').size()<1) {
-					var $tbody = this.$table.find('tbody tr:first').children();
-				    this.widthcol = $tbody.map(function() {
-				        return $(this).width();
-				    }).get();
+				var $active_view=$("div.oe_view_manager_current[data-view-type='list']");
+				var $tbody = this.$table.find('tbody tr:first').children();
+			    this.widthcol = $tbody.map(function() {
+			        return $(this).width();
+			    }).get();
+				if($active_view.find('.oe_view_manager_view_list table.oe_list_header_custom:first').size()<1) {
+					
 					console.log($('.oe_view_manager_view_list table.oe_list_header_custom:first').size());
 					this.$table.before("<table class='oe_list_header_custom'></table>");
 					this.$header=$('.oe_view_manager_view_list table.oe_list_header_custom:first');
+					this.$header.after("<div class='fixedwrapper fixed_headers'></div>");
+					$('div.fixedwrapper').append(this.$table);
 					var $thead=this.$table.find('thead');
 					this.$header.append($thead.clone());
 					$thead.css({'visibility':'collapse'});
-					
-					this.$header.find('tr').children().each(function(i, v) {
-				    	$(v).width(self.widthcol[i]);
-				    });
-					
-					this.$table.height($(document).find("div.oe_view_manager_body:first").height()-this.$header.height()-2);
+					//console.log($active_view.find("div.oe_view_manager_body:first"));
 				
 				}
-			    console.log("resize");
+				this.$header=$('.oe_view_manager_view_list table.oe_list_header_custom:first');
+			    this.$header.find('tr').children().each(function(i, v) {
+			    	$(v).width(self.widthcol[i]-1);
+			    });
+			    $('div.fixedwrapper').height($active_view.find("div.oe_view_manager_body:first").height()-this.$header.height()-2);
+				$active_view.find('.oe_view_manager_view_list table.oe_list_header_custom:first').css({"display":this.$table.css("display")});
+			    //console.log("resize");
 			} else {
 				var self=this;
 				window.setTimeout(function() {
@@ -54,5 +56,6 @@ openerp.web_fixed_headers = function(instance) {
 	    }
 	    
 	});
+
 }
 
