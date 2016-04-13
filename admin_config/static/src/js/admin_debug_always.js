5f6e6e69d858077b8f3a3b5b431eedd96bfcd278
asalaun <alexandre.salaun@ndp-systemes.fr>
2016-04-13 17:43:50 +0200

    mise à niveau v9.0

---
+++ admin_config/static/src/js/admin_debug_always.js
@@ -0,0 +1,30 @@
+odoo.define('web.DebugManager_always', function (require) {
+   "use strict";
+   	
+   var core = require('web.core');
+   var $ = require('$');
+   var utils = require('web.utils');
+   var session = require('web.session');
+   var debug=require('web.DebugManager');
+   var SystrayMenu = require('web.SystrayMenu');
+   var _t = core._t;
+   var QWeb = core.qweb;
+
+   var debug = $.deparam($.param.querystring()).debug !== undefined;
+   if(!debug) {
+	   var wid= require('web.Widget').extend({
+		   start: function() {
+	           if(session.uid === 1){
+	        	   window.location="?debug=";
+	           }
+	       }
+	   });
+	   
+	   SystrayMenu.Items.push(wid);
+   
+	   return wid;
+   }
+});
+
+
+
