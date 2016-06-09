7f3531a682f95b45b7fd93c69c7601e3974f3ab8
asalaun <alexandre.salaun@ndp-systemes.fr>
2016-06-09 17:51:30 +0200

    odoo-addons/sirail#411
    aggregation des move dans move_to
    optimisation de la fonction de stock.picking top_level

--- stock_quant_packages_moving_wizard/models/stock.py
+++ stock_quant_packages_moving_wizard/models/stock.py
@@ -17,7 +17,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from datetime import datetime
 
 from openerp import models, fields, api, _
 
@@ -90,6 +89,7 @@ class StockQuant(models.Model):
             if not is_manual_op:
                 new_picking.do_transfer()
         return move_recordset
+
 
 class StockWarehouse(models.Model):
     _inherit = 'stock.warehouse'
