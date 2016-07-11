461e6714bc950dbfb60e0810483203617e0b52b6
Thomas Lemaire <thomas.lemaire@ndp-systemes.fr>
2016-07-11 17:36:52 +0200

    Retrait des print

--- purchase_group_by_period/tests/test_get_start_end_date.py
+++ purchase_group_by_period/tests/test_get_start_end_date.py
@@ -10,12 +10,9 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
     WEEKS = 'weeks'
 
     def test_3_month(self):
-        print "----------------3 Month---------------"
         date = datetime(2015, 8, 12)
         tf = self._get_tf(3, self.MONTHS)
         date_start, date_end = tf.get_start_end_dates(date)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2015)
         self.assertEqual(date_start.month, 07)
         self.assertEqual(date_start.day, 01)
@@ -31,12 +28,9 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_2_month(self):
-        print "----------------2 Month---------------"
         date = datetime(2016, 6, 30)
         tf = self._get_tf(2, self.MONTHS)
         date_start, date_end = tf.get_start_end_dates(date)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 5)
         self.assertEqual(date_start.day, 01)
@@ -52,12 +46,9 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_1_year(self):
-        print "----------------1 Year---------------"
         date = datetime(2016, 7, 3)
         tf = self._get_tf(1, self.YEARS)
         date_start, date_end = tf.get_start_end_dates(date)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 01)
         self.assertEqual(date_start.day, 01)
@@ -73,12 +64,9 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_2_year(self):
-        print "----------------2 Years---------------"
         date = datetime(2016, 7, 3)
         tf = self._get_tf(2, self.YEARS)
         date_start, date_end = tf.get_start_end_dates(date)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2015)
         self.assertEqual(date_start.month, 01)
         self.assertEqual(date_start.day, 01)
@@ -94,12 +82,9 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_10_days(self):
-        print "----------------10 days---------------"
         date = datetime(2016, 8, 12)
         tf = self._get_tf(10, self.DAYS)
         date_start, date_end = tf.get_start_end_dates(date)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 8)
         self.assertEqual(date_start.day, 8)
@@ -115,12 +100,9 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_20_days(self):
-        print "----------------20 days---------------"
         date = datetime(2016, 7, 3)
         tf = self._get_tf(20, self.DAYS)
         date_start, date_end = tf.get_start_end_dates(date)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 06)
         self.assertEqual(date_start.day, 29)
@@ -136,12 +118,9 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_2_week(self):
-        print "----------------2 weeks---------------"
         date = datetime(2016, 7, 3)
         tf = self._get_tf(2, self.WEEKS)
         date_start, date_end = tf.get_start_end_dates(date)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 06)
         self.assertEqual(date_start.day, 20)
@@ -157,13 +136,10 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_2_month_custom1(self):
-        print "----------------2 Month custom-1 ---------------"
         date = datetime(2016, 8, 12)
         date_reference = datetime(2016, 7, 15)
         tf = self._get_tf(2, self.MONTHS)
         date_start, date_end = tf.get_start_end_dates(date, date_reference)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 07)
         self.assertEqual(date_start.day, 15)
@@ -179,13 +155,10 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_2_month_custom2(self):
-        print "----------------2 Month custom-2 ---------------"
         date = datetime(2016, 7, 7)
         date_reference = datetime(2016, 7, 15)
         tf = self._get_tf(2, self.MONTHS)
         date_start, date_end = tf.get_start_end_dates(date, date_reference)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 05)
         self.assertEqual(date_start.day, 15)
@@ -201,13 +174,10 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_10_days_custom1(self):
-        print "----------------10 days custom-1 ---------------"
         date = datetime(2016, 8, 12)
         date_reference = datetime(2016, 7, 15)
         tf = self._get_tf(10, self.DAYS)
         date_start, date_end = tf.get_start_end_dates(date, date_reference)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 8)
         self.assertEqual(date_start.day, 5)
@@ -223,13 +193,10 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_10_days_custom2(self):
-        print "----------------10 days custom-2 ---------------"
         date = datetime(2016, 7, 7)
         date_reference = datetime(2016, 7, 15)
         tf = self._get_tf(10, self.DAYS)
         date_start, date_end = tf.get_start_end_dates(date, date_reference)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 07)
         self.assertEqual(date_start.day, 6)
@@ -245,13 +212,10 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_2_weeks_custom1(self):
-        print "----------------2 weeks custom-1 ---------------"
         date = datetime(2016, 8, 12)
         date_reference = datetime(2016, 7, 15)
         tf = self._get_tf(2, self.WEEKS)
         date_start, date_end = tf.get_start_end_dates(date, date_reference)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 7)
         self.assertEqual(date_start.day, 30)
@@ -267,13 +231,10 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_2_weeks_custom2(self):
-        print "----------------2 weeks custom-2 ---------------"
         date = datetime(2016, 7, 7)
         date_reference = datetime(2016, 7, 15)
         tf = self._get_tf(2, self.WEEKS)
         date_start, date_end = tf.get_start_end_dates(date, date_reference)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 07)
         self.assertEqual(date_start.day, 2)
@@ -289,13 +250,10 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.second, 59)
 
     def test_2_years_custom1(self):
-        print "----------------2 years custom-1 ---------------"
         date = datetime(2016, 8, 12)
         date_reference = datetime(2016, 7, 15)
         tf = self._get_tf(2, self.YEARS)
         date_start, date_end = tf.get_start_end_dates(date, date_reference)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2016)
         self.assertEqual(date_start.month, 7)
         self.assertEqual(date_start.day, 15)
@@ -305,13 +263,10 @@ class TestPurchaseGroupByPeriod(common.TransactionCase):
         self.assertEqual(date_end.day, 15)
 
     def test_2_years_custom2(self):
-        print "----------------2 years custom-2 ---------------"
         date = datetime(2016, 7, 7)
         date_reference = datetime(2016, 7, 15)
         tf = self._get_tf(2, self.YEARS)
         date_start, date_end = tf.get_start_end_dates(date, date_reference)
-        print "Date start [%s]" % date_start
-        print "Date end [%s]" % date_end
         self.assertEqual(date_start.year, 2014)
         self.assertEqual(date_start.month, 7)
         self.assertEqual(date_start.day, 16)
