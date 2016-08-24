7f1f62aac91a5f787efc5c9a546257dc9541b5d7
Thomas Lemaire <thomas.lemaire@ndp-systemes.fr>
2016-08-24 15:14:25 +0200

    Ajout stock dans dépendances stock_performance_improved

--- stock_performance_improved/__openerp__.py
+++ stock_performance_improved/__openerp__.py
@@ -23,7 +23,7 @@
     'author': 'NDP Systèmes',
     'maintainer': 'NDP Systèmes',
     'category': 'Technical Settings',
-    'depends': ['scheduler_async'],
+    'depends': ['scheduler_async', 'stock'],
     'description': """
 Stock Performance Improved
 ==========================
