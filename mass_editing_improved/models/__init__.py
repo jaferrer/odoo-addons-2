47307138107bbfc105d474607e2203c14d45c61a
Alexis Pasquier <alexis.pasquier@ndp-systemes.fr>
2016-08-31 17:29:28 +0200

    odoo-addons/entrepot#277
    Modification de l'année du copyright
    Suppression de commentaire

--- mass_editing_improved/models/__init__.py
+++ mass_editing_improved/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2015 NDP Systèmes (<http://www.ndp-systemes.fr>).
+#    Copyright (C) 2016 NDP Systèmes (<http://www.ndp-systemes.fr>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
