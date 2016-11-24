c2171ee55fa3c9ffd13f29dbb98d476d461f011d
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2016-11-24 19:14:15 +0100

    Add information on negative quant that is about to be created

--- stock_forbid_negative_quants/stock_forbid_negative_quants.py
+++ stock_forbid_negative_quants/stock_forbid_negative_quants.py
@@ -29,7 +29,15 @@ class StockQuant(models.Model):
                       force_location_from=False, force_location_to=False):
         if not config["test_enable"] and move.location_id.usage == 'internal':
             raise exceptions.except_orm(_("Error !"),
-                                        _("You are not allowed to move products quants that are not available."
-                                          "If the quants are available, check that package, owner and lot no. match"))
+                                        _("You are not allowed to move products quants that are not available. "
+                                          "If the quants are available, check that package, owner and lot no. match. "
+                                          "Product: %s, Missing qty: %s, Location: %s, Lot: %s, Package: %s") % (
+                                            move.product_id.default_code,
+                                            qty,
+                                            move.location_id.complete_name,
+                                            lot_id and lot_id.name or "-",
+                                            src_package_id and src_package_id.name or "-",
+                                            )
+                                        )
         return super(StockQuant, self)._quant_create(qty, move, lot_id, owner_id, src_package_id, dest_package_id,
                                                      force_location_from, force_location_to)
