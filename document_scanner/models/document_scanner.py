68d05552176b023ce527f75652395b7c88443c7b
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2017-04-25 17:45:43 +0200

    Various fixes on document_scanner

--- document_scanner/models/document_scanner.py
+++ document_scanner/models/document_scanner.py
@@ -49,6 +49,12 @@ class IrAttachmentScanner(models.Model):
         self.env['bus.bus'].sendone(channel=channel, message=msg)
         return {'scan_name': user.scanner_id.name}
 
+    @api.multi
+    def check(self, mode, values=None):
+        if not self.env.user.has_group('document_scanner.bot_ir_attachment'):
+            super(IrAttachmentScanner, self).check(mode, values=values)
+
+
 class ImBusScanner(models.Model):
     _inherit = 'bus.bus'
 
@@ -58,8 +64,8 @@ class ImBusScanner(models.Model):
         return self.env.cr.fetchone()
 
 
-
 class DocumentScannerBus(openerp.addons.bus.bus.Controller):
+
     def _poll(self, dbname, channels, last, options):
         if request.session.uid:
             channels.append((request.db, 'request.scanner'))
