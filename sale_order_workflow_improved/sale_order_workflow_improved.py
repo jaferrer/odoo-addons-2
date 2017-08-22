3594970b3c8fc5ba3aa394b1496a99adf3cdbe95
Alexis Pasquier <alexis.pasquier@ndp-systemes.fr>
2017-08-22 09:01:08 +0200

    Ajout d'un module v8, et passage en v9

---
+++ sale_order_workflow_improved/sale_order_workflow_improved.py
@@ -0,0 +1,35 @@
+# -*- coding: utf8 -*-
+#
+#    Copyright (C) 2015 NDP Systèmes (<http://www.ndp-systemes.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from openerp import models, fields, api, exceptions, _
+
+
+class QuantitiesModificationsSaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    workflow_done = fields.Boolean(string="Done state reached in workflow")
+    state = fields.Selection(track_visibility='onchange')
+
+    @api.multi
+    def reopen_order(self):
+        if any([order.state != 'done' for order in self]):
+            raise exceptions.except_orm(_("Error!"), _("Impossible to reopen a not done sale order."))
+        self.write({'workflow_done': True,
+                    'state': 'sale'})
+        self.message_post(body=_("Sale order reopened by %s") % self.env.user.name)
