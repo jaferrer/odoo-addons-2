d1f0028951692da6ef88426b7ca46319211b0b43
asalaun <alexandre.salaun@ndp-systemes.fr>
2017-11-22 14:42:56 +0100

    ajout du module v8 du champs calcul en bdd

---
+++ computed_field_database/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8 -*-
+#
+# Copyright (C) 2017 NDP Systèmes (<http://www.ndp-systemes.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+{
+    'name': 'Computed Field database',
+    'version': '0.1',
+    'author': 'NDP Systèmes',
+    'maintainer': 'NDP Systèmes',
+    'category': 'Technical',
+    'depends': ['base'],
+    'description': """
+Computed field database
+========================
+This module create a compute field function in database.
+""",
+    'website': 'http://www.ndp-systemes.fr',
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+    'application': False,
+    'sequence': 999,
+}
