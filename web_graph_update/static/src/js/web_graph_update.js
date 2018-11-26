ea5207180b95c9d30d7eb3fc0a5e4b6856b77778
asalaun <alexandre.salaun@ndp-systemes.fr>
2018-11-26 15:06:05 +0100

    correction et ajout fonctionnalité sort muli colonne

--- web_graph_update/static/src/js/web_graph_update.js
+++ web_graph_update/static/src/js/web_graph_update.js
@@ -18,14 +18,15 @@ openerp.web_graph_update = function(instance) {
 		draw_measure_row: function (measure_row) {
 	        var $row = $('<tr>').append('<th>');
 	        var self=this;
-	        _.each(measure_row, function (cell) {
+	        _.each(measure_row, function (cell,index) {
 	        	_.each(self.measure_list,function(item){
+	        	    console.log(index);
 	        		if(item.string==cell.text) {
 		        		var $cell = $('<th>').addClass('measure_row').addClass('oe_sortable').attr('data-id',
 		        				(self.pivot.sort!=null && self.pivot.sort[0].indexOf(item.field)>=0 && self.pivot.sort[0].indexOf('-') == -1)?
 		        						"-"+item.field:item.field).append("<div>"+cell.text+"</div>");
 			            if (cell.is_bold) {$cell.css('font-weight', 'bold');}
-			            if (self.pivot.sort!=null && self.pivot.sort[0].indexOf(item.field)>=0) {
+			            if (self.pivot.sort!=null && self.pivot.sort[0].indexOf(item.field)>=0 && index == self.pivot.index_sort) {
 			            	$cell.addClass((self.pivot.sort[0].indexOf('-') == -1)?"sortup":"sortdown");
 			            }
 			            $row.append($cell);
@@ -39,22 +40,64 @@ openerp.web_graph_update = function(instance) {
             e.stopPropagation();
             var $column = $(e.currentTarget);
             var col_name = $column.data('id');
+            this.pivot.index_sort = $column[0].cellIndex - 1;
+            sort=[]
+            _.each(this.pivot.cols.groupby, function (col) {
+				sort.push(col.field);
+            });
+
             this.pivot.sort=[col_name];
+            this.pivot.sort_group=sort;
             this.pivot.update_data().then(this.proxy('display_data'));
+        },
+
+    draw_rows: function (rows, doc_fragment, frozen_rows) {
+        var rows_length = rows.length,
+            $tbody = $('<tbody>');
+
+        doc_fragment.append($tbody);
+        var self = this;
+
+        if(this.pivot.index_sort !=null) {
+
+            rows.sort(function compare(a, b) {
+                comp = (self.pivot.sort[0].indexOf('-') == -1)?-1:1;
+                var genreA = parseFloat(a.cells[self.pivot.index_sort].value.toUpperCase().replace(",", ".").replace(/\s/g, ""));
+                var genreB = parseFloat(b.cells[self.pivot.index_sort].value.toUpperCase().replace(",", ".").replace(/\s/g, ""));
+                if (a.cells[self.pivot.index_sort].value == "") {
+                    genreA = null;
+                }
+                if (b.cells[self.pivot.index_sort].value == "") {
+                    genreB = null;
+                }
+                var comparison = 0;
+                if ((genreA != null && genreB == null) || (genreA != null && genreB != null && genreA > genreB)) {
+                    comparison = -1*comp;
+                } else if ((genreA == null && genreB != null) || (genreA != null && genreB != null && genreA < genreB)) {
+                    comparison = 1*comp;
+                }
+                return comparison;
+            });
         }
+        for (var i = 0; i < rows_length; i++) {
+            $tbody.append(this.draw_row(rows[i], frozen_rows));
+        }
+    }
 	    
 	});
 
     instance.web_graph.PivotTable.include({
     	
     	sort : null,
+		sort_group: [],
+        index_sort:null,
     	
     	get_groups: function (groupbys, fields, domain) {
             var self = this;
             var result = this.model.query(_.without(fields, '__count'));
-            if(this.sort!=null) {
-            	result=result.order_by(this.sort);
-            }
+            /*if(this.sort!=null) {
+            	result=result.order_by(this.sort_group.concat(this.sort));
+            }*/
 			return result.filter(domain)
 			.context(this.context)
 			.lazy(false)
