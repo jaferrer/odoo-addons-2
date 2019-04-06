538891670bad63f27a55ac7d35dcfba42d7ab826
Alexis Pasquier <alexis.pasquier@ndp-systemes.fr>
2019-04-06 15:29:26 +0200

    Ajout module pour gerer/afficher les jour de la semaine

---
+++ res_calendar_day/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf8 -*-
+#
+# Copyright (C) 2019 NDP Systèmes (<http://www.ndp-systemes.fr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+{
+    'name': "Ressource Calendar Days",
+    'version': '0.1',
+    'author': 'NDP Systèmes',
+    'maintainer': 'NDP Systèmes',
+    'category': 'Technical Settings',
+    'depends': [],
+    'description': "Useful to get a list of field in a M2O, O2M or M2M",
+    'website': 'http://www.ndp-systemes.fr',
+    'data': [
+        'security/ir.model.access.csv',
+        'res.calendar.day.csv',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
