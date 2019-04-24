1c94d44b6a413369f293c3f21e1873d53e76ccc2
Julien <julien.pozzani@ndp-systemes.fr>
2019-04-24 17:01:16 +0200

    WIP

--- view_form_improved/static/src/js/view_form.js
+++ view_form_improved/static/src/js/view_form.js
@@ -1,5 +1,5 @@
 'use strict';
-openerp.view_form_improved = function(instance) {
+openerp.view_form_improved = function (instance) {
     /**
      * Prevents read request for one2many fields when invisible.
      */
@@ -7,12 +7,23 @@ openerp.view_form_improved = function(instance) {
     // instance.web.BufferedDataSet = instance.web.BufferedDataSet.extend({
     instance.web.BufferedDataSet.include({
         read_ids: function (ids, fields, options) {
-            let isHidden = (this.o2m !== undefined) && $.inArray("oe_form_invisible", this.o2m.el.classList) !== -1;
-            if (isHidden) {
-                console.log("[BufferedDataSet.invisible]" + this.model + "." + this.o2m.name);
-                return this._super([], fields, options)
+            var res = false;
+            // this._check_visibility();
+            if (this.hasOwnProperty('o2m')) {
+                console.log("this.o2m.el.classList : ", this.o2m.el.classList);
+                this.o2m._check_visibility();
+                console.log("After visibility: ", this.o2m.el.classList);
+                if ($.inArray("oe_form_invisible", this.o2m.el.classList) !== -1) {
+                    console.log("____NO LOAD : ",  this.child_name, this.o2m.el.classList, ids, arguments);
+                    arguments[0] = [];
+                    res =  this._super.apply(this, arguments);
+                }
+                console.log("----LOAD : ",  this.child_name, this.o2m.el.classList, ids, arguments);
             }
-            return this._super(ids, fields, options)
+            res = this._super.apply(this, arguments);
+
+            console.log('res : ', res, this.o2m.el.classList)
+            return res
         }
     });
     //instance.web.BufferedDataSet.virtual_id_regex = /^one2many_v_id_.*$/;
