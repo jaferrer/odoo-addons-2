79ece9a6595a4df6ab23c23b15af567cbee43c53
Julien <julien.pozzani@ndp-systemes.fr>
2019-04-25 09:32:02 +0200

    Toujours soucis oe_invisible async

--- view_form_improved/static/src/js/view_form.js
+++ view_form_improved/static/src/js/view_form.js
@@ -7,23 +7,31 @@ openerp.view_form_improved = function (instance) {
     // instance.web.BufferedDataSet = instance.web.BufferedDataSet.extend({
     instance.web.BufferedDataSet.include({
         read_ids: function (ids, fields, options) {
-            var res = false;
-            // this._check_visibility();
+            var self = this;
             if (this.hasOwnProperty('o2m')) {
-                console.log("this.o2m.el.classList : ", this.o2m.el.classList);
-                this.o2m._check_visibility();
-                console.log("After visibility: ", this.o2m.el.classList);
-                if ($.inArray("oe_form_invisible", this.o2m.el.classList) !== -1) {
-                    console.log("____NO LOAD : ",  this.child_name, this.o2m.el.classList, ids, arguments);
-                    arguments[0] = [];
-                    res =  this._super.apply(this, arguments);
-                }
-                console.log("----LOAD : ",  this.child_name, this.o2m.el.classList, ids, arguments);
-            }
-            res = this._super.apply(this, arguments);
+                console.log("-------------------------------- this.o2m : ", this.child_name, ids);
+                arguments[0] = [];
+                var t = 0;
+                var args = arguments;
+                console.log("BEFORE SUPER : ", this.o2m.el.classList, $.inArray("oe_form_invisible", this.o2m.el.classList) === -1);
+                var res = false;
+                res = this._super.apply(this, arguments).done(function () {
 
-            console.log('res : ', res, this.o2m.el.classList)
-            return res
+                    console.log("AFTER SUPER : ", self.o2m.el.classList, $.inArray("oe_form_invisible", self.o2m.el.classList));
+                    if ($.inArray("oe_form_invisible", self.o2m.el.classList) === -1) {
+                        console.log("READ : ", ids);
+                        args[0] = ids;
+                        res = self._super.apply(self, args);
+                        console.log("sub res : ", res)
+                    }
+                    console.log("Final SUPER : ", self.o2m.el.classList, $.inArray("oe_form_invisible", self.o2m.el.classList));
+                });
+                console.log("res: ", res)
+                return res
+            }
+            else{
+                return self._super.apply(this, arguments);
+            }
         }
     });
     //instance.web.BufferedDataSet.virtual_id_regex = /^one2many_v_id_.*$/;
