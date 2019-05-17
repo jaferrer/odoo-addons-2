099b518fc4529fa1587963fd68d5debefbf5ced9
paul <paul.boutin@ndp-systemes.fr>
2019-05-17 15:02:56 +0200

    odoo-addons/sirail-phase2#74 Bus - Séparation des notions de création / édition à l'import

--- bus_integration/models/mapping_configuration_helper.py
+++ bus_integration/models/mapping_configuration_helper.py
@@ -49,7 +49,8 @@ class MappingConfigurationHelper(models.TransientModel):
                         'field_id': mapping_field.field_id.id,
                         'map_name': mapping_field.map_name,
                         'export_field': mapping_field.export_field,
-                        'import_field': mapping_field.import_field,
+                        'import_creatable_field': mapping_field.import_creatable_field,
+                        'import_updatable_field': mapping_field.import_updatable_field,
                         'is_migration_key': mapping_field.is_migration_key,
                     })]
             rec.helper_line_ids = helper_line_ids
@@ -71,7 +72,8 @@ class MappingConfigurationHelper(models.TransientModel):
                 'field_id': field.id,
                 'map_name': field.name,
                 'export_field': self.is_exportable,
-                'import_field': self.is_importable,
+                'import_creatable_field': self.is_importable,
+                'import_updatable_field': self.is_importable,
                 'is_migration_key': False,
             })
 
@@ -87,39 +89,35 @@ class MappingConfigurationHelper(models.TransientModel):
     @api.multi
     def validate(self):
         self.ensure_one()
-        model_configuration_header = u"id,model_id:id,key_xml_id,deactivated_sync,deactivate_on_delete,is_exportable," \
-                                     u"is_importable"
-        fields_configuration_header = u"id,mapping_id:id,field_id:id,map_name,export_field,import_field," \
-                                      u"is_migration_key"
         model_mapping_xml_id = u"mapping_model_%s" % (self.model_id.model.replace('.', '_'))
         model_xml_id = self.model_id.get_external_id().get(self.model_id.id)
         model_configuration = u"%s,%s,%s,%s,%s,%s,%s" % (model_mapping_xml_id,
                                                          model_xml_id,
+                                                         self.is_exportable,
+                                                         self.is_importable,
                                                          self.key_xml_id,
                                                          self.deactivated_sync,
-                                                         self.deactivate_on_delete,
-                                                         self.is_exportable,
-                                                         self.is_importable)
+                                                         self.deactivate_on_delete)
         fields_configuration = u""""""
         for wizard_line in self.helper_line_ids:
             field_mapping_xml_id = u"mapping_field_%s_%s" % (self.model_id.model.replace('.', '_'),
                                                              wizard_line.field_id.name)
             field_xml_id = wizard_line.field_id.get_external_id().get(wizard_line.field_id.id)
-            fields_configuration += u"""%s,%s,%s,%s,%s,%s,%s""" % (field_mapping_xml_id,
-                                                                   model_mapping_xml_id,
-                                                                   field_xml_id,
-                                                                   wizard_line.map_name,
-                                                                   wizard_line.export_field,
-                                                                   wizard_line.import_field,
-                                                                   wizard_line.is_migration_key)
+            fields_configuration += u"""%s,%s,%s,%s,%s,%s,%s,%s""" % (field_mapping_xml_id,
+                                                                      model_mapping_xml_id,
+                                                                      field_xml_id,
+                                                                      wizard_line.map_name,
+                                                                      wizard_line.export_field,
+                                                                      wizard_line.import_creatable_field,
+                                                                      wizard_line.import_updatable_field,
+                                                                      wizard_line.is_migration_key)
             if wizard_line != self.helper_line_ids[-1]:
                 fields_configuration += u"""\n"""
         vals = {
-            'model_configuration_header': model_configuration_header,
             'model_configuration': model_configuration,
-            'fields_configuration_header': fields_configuration_header,
             'fields_configuration': fields_configuration,
         }
+
         answer = self.env['mapping.configuration.helper.answer'].create(vals)
         return {
             'name': u"Mapping configuration helper",
@@ -150,7 +148,19 @@ class MappingConfigurationHelperLine(models.TransientModel):
 class MappingConfigurationHelperAnswer(models.TransientModel):
     _name = 'mapping.configuration.helper.answer'
 
-    model_configuration_header = fields.Char(string=u"Model configuration header")
-    fields_configuration_header = fields.Char(string=u"Fields configuration header")
+    model_configuration_header = fields.Char(string=u"Model configuration header",
+                                             compute="_compute_static_fields",
+                                             store=False, readonly=True)
+    fields_configuration_header = fields.Char(string=u"Fields configuration header",
+                                              compute="_compute_static_fields",
+                                              store=False, readonly=True)
+
     model_configuration = fields.Char(string=u"Model configuration")
     fields_configuration = fields.Text(string=u"Fields configuration")
+
+    def _compute_static_fields(self):
+        for rec in self:
+            rec.fields_configuration_header = u"id,mapping_id:id,field_id:id,map_name,export_field," \
+                                              u"import_creatable_field,import_updatable_field,is_migration_key"
+            rec.model_configuration_header = u"id,model_id:id,is_exportable,is_importable,key_xml_id,deactivated_sync,"\
+                                             u"deactivate_on_delete"
