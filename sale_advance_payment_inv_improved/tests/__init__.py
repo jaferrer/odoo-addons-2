16bb526f42815819b9212222df03b5af57d19ef2
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2019-06-04 10:41:04 +0200

    Branche 11.0

--- sale_advance_payment_inv_improved/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of NDP Systemes. See LICENSE file for full copyright and licensing details.
-
-import odoo.addons.sale.tests.test_sale_to_invoice
-import odoo.addons.sale.tests.test_sale_order
-import odoo.addons.sale.tests.test_product_id_change
-import odoo.addons.sale.tests.test_sale_ui
