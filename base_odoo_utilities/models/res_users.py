5fd875ef8a98373ead5d585a168f98024f80f6a9
abraine <alexis.braine@ndp-systemes.fr>
2019-06-12 16:38:45 +0200

    Déplacement des méthodes format_local_date* dans res.partner - odoo-addons/AEF#289

--- base_odoo_utilities/models/res_users.py
+++ base_odoo_utilities/models/res_users.py
@@ -22,18 +22,29 @@ import datetime
 from odoo import fields, models
 
 
-class ResUsers(models.Model):
-    _inherit = 'res.users'
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
 
     def format_local_date(self, date=None):
         date_py = date or datetime.date.today()
         if isinstance(date_py, (str, unicode)):
             date_py = fields.Date.from_string(date_py)
-        return date_py.strftime(self.env['res.lang'].search([('code', '=', self.env.user.lang)]).date_format)
+        lang = self.env['res.lang'].search([('code', '=', self.first().lang or self.env.user.lang)])
+        return date_py.strftime(lang.date_format)
 
     def format_local_datetime(self, date=None):
         date_py = date or datetime.datetime.now()
         if isinstance(date_py, (str, unicode)):
             date_py = fields.Datetime.from_string(date_py)
-        lang = self.env['res.lang'].search([('code', '=', self.env.user.lang)])
+        lang = self.env['res.lang'].search([('code', '=', self.first().lang or self.env.user.lang)])
         return date_py.strftime("%s %s" % (lang.date_format, lang.time_format))
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    def format_local_date(self, date=None):
+        self.partner_id.format_local_date(date)
+
+    def format_local_datetime(self, date=None):
+        self.partner_id.format_local_datetime(date)
