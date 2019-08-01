cccfc278d380ede2b642777e0e34a3d2e5b4740d
Kevin <kevin.fronteau@ndp-systemes.fr>
2019-08-01 10:17:10 +0200

    Creation du module Backend Theme Extension m14/openerp/m14#26

---
+++ backend_theme_extension/__manifest__.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+{
+    'name': 'Backend Theme Extension classes and fixes',
+    'version': '1.0',
+    'author': 'NDP Systèmes',
+    'website': 'http://www.m14.fr',
+    'category': 'Generic Modules',
+    'description': """
+Module Backend Theme Extension
+==============================
+Ajout de styles supplémentaires et divers fixes pour les applications Odoo developpées par NDP Systèmes
+    """,
+    'depends': ['web'],
+    'data': [
+        'views/assets.xml'
+    ],
+    'images': ['static/description/logo.png'],
+    'installable': True,
+    'application': True,
+}
