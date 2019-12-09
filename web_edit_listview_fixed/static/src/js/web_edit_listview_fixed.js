5fc08c1245606f278b8c7c7595b6171167ddcc48
Julien <julien.pozzani@ndp-systemes.fr>
2019-12-09 15:21:43 +0100

    odoo-addons/reanova#243	Correction bug dans vue modal

--- web_edit_listview_fixed/static/src/js/web_edit_listview_fixed.js
+++ web_edit_listview_fixed/static/src/js/web_edit_listview_fixed.js
@@ -1,20 +1,13 @@
 odoo.define('web.EditListFormFixed', function (require) {
     "use strict";
 
-    var core = require('web.core');
-    var data = require('web.data');
-    var FormView = require('web.FormView');
-    var common = require('web.list_common');
     var ListView = require('web.ListView');
-    var utils = require('web.utils');
-    var Widget = require('web.Widget');
-
     ListView.include({
 
         resize_field: function (field, cell) {
             var result = this._super.apply(this, arguments);
             var pos_ok = $('.oe_edition')[0];
-             if (pos_ok !== 'undefined' && pos_ok !== undefined) {
+             if (pos_ok !== 'undefined' && pos_ok !== undefined && field.$el.parents('.modal-dialog').length == 0) {
                 field.$el.css({'top': pos_ok.offsetTop});
             }
             return result;
