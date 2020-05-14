6e49f9e60517bf6159d9646f4fbebb4750b04182
Alexis Pasquier <alexis.pasquier@ndp-systemes.fr>
2020-05-14 17:52:25 +0200

    Ajout code APE - odoo-addons/radiscapucine#59

--- currency_rate_update/migrations/12.0.1.1.1/pre-migration.py
+++ currency_rate_update/migrations/12.0.1.1.1/pre-migration.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
-_field_renames = [
+_FIELD_RENAMES = [
     ('res.company', 'res_company', 'auto_currency_up',
      'currency_rates_autoupdate'),
 ]
 
-xmlid_renames = [
+XMLID_RENAMES = [
     ('currency_rate_update.currency_rate_update_service_multicompany_rule',
      'currency_rate_update.res_currency_rate_provider_multicompany'),
 ]
@@ -15,5 +15,5 @@ xmlid_renames = [
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.rename_fields(env, _field_renames)
-    openupgrade.rename_xmlids(env.cr, xmlid_renames)
+    openupgrade.rename_fields(env, _FIELD_RENAMES)
+    openupgrade.rename_xmlids(env.cr, XMLID_RENAMES)
