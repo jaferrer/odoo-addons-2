7fb9b9f571048f199658608f8ad34f3125d02448
sfromont <sfromont@ndp-systemes.fr>
2020-08-28 12:18:21 +0200

    odoo-addons/sirail#2885 purchase order field switch to new api in odoo 200821-1760 - Total HT à 0 dans certaines PA

--- purchase_performance_improved/purchase_performance_improved.py
+++ purchase_performance_improved/purchase_performance_improved.py
@@ -24,42 +24,6 @@ from openerp import models, api
 
 class Purchase(osv.osv):
 
-    def _amount_all_improved(self, cr, uid, ids, field_name, arg, context=None):
-        result = {}
-        for order_id in ids:
-            order = self.pool.get('purchase.order').browse(cr, uid, order_id, context=context)
-            target_data = self.pool.get('purchase.order')._amount_all(cr, uid, order_id, field_name, arg, context=context)
-            order_data = {'amount_tax': order.amount_tax,
-                          'amount_untaxed': order.amount_untaxed,
-                          'amount_total': order.amount_total}
-            if order_data != target_data.get(order_id):
-                result[order_id] = target_data.get(order_id)
-        return result
-
-    def _get_order_improved(self, cr, uid, ids, context=None):
-        result = self.pool.get('purchase.order')._get_order(cr, uid, ids, context=context)
-        return result
-
-    _columns = {
-        'amount_untaxed': fields.function(_amount_all_improved, digits_compute=dp.get_precision('Account'), string='Untaxed Amount',
-                                          store={
-            'purchase.order.line': (_get_order_improved, ['product_qty', 'taxes_id',
-                                                          'product_uom', 'product_id', 'price_unit',
-                                                          'order_id'], 10),
-        }, multi="sums", help="The amount without tax", track_visibility='always'),
-        'amount_tax': fields.function(_amount_all_improved, digits_compute=dp.get_precision('Account'), string='Taxes',
-                                      store={
-            'purchase.order.line': (_get_order_improved, ['product_qty', 'taxes_id',
-                                                          'product_uom', 'product_id', 'price_unit',
-                                                          'order_id'], 10),
-        }, multi="sums", help="The tax amount"),
-        'amount_total': fields.function(_amount_all_improved, digits_compute=dp.get_precision('Account'), string='Total',
-                                        store={
-            'purchase.order.line': (_get_order_improved, ['product_qty', 'taxes_id',
-                                                          'product_uom', 'product_id', 'price_unit',
-                                                          'order_id'], 10),
-        }, multi="sums", help="The total amount")
-    }
     _inherit = ['purchase.order']
     _description = "Purchase Order"
     _order = 'date_order desc, id desc'
