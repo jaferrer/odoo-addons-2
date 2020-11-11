1c0c982d710bc625f82c3ded81200beb1f92604a
tlemaire <thomas.lemaire@ndp-systemes.fr>
2020-11-11 08:45:57 +0100

    Cf issue odoo-addons/institut-notre-dame-esperance#17 : l'heure de début et de fin sont obligatoires dans les séances

--- training_base/models/training_sitting.py
+++ training_base/models/training_sitting.py
@@ -34,7 +34,8 @@ class TrainingSitting(models.Model):
     training_id = fields.Many2one('training.training', string="Training", related='session_id.training_id',
                                   readonly=True, store=True)
     trainer_id = fields.Many2one('res.partner', string="Trainer", required=True)
-    location_id = fields.Many2one('res.partner', string="Location", required=True)
+    location_id = fields.Many2one('res.partner', string="Location", required=True,
+                                  domain=[('is_training_location', '=', True)])
     date = fields.Date(string="Date", required=True)
     start_hour = fields.Float(string="Start Hour", required=True)
     end_hour = fields.Float(string="End Hour", required=True)
@@ -50,6 +51,13 @@ class TrainingSitting(models.Model):
         for rec in self:
             rec.formatted_start_hour = format_duration(rec.start_hour)
             rec.formatted_end_hour = format_duration(rec.end_hour)
+
+    @api.onchange('session_id')
+    def onchange_session_id(self):
+        for rec in self:
+            if rec.session_id:
+                rec.trainer_id = rec.session_id.trainer_id
+                rec.location_id = rec.session_id.location_id
 
     def send_convocation_to_all(self):
         for rec in self:
@@ -73,22 +81,22 @@ class TrainingSitting(models.Model):
             if self.search(domain + [('id', 'not in', sittings_before.ids), ('id', 'not in', sittings_after.ids)]):
                 raise exceptions.UserError(_("Two sittings cannot happen simultaneously"))
 
-    @api.onchange('session_id')
-    def onchange_session_id(self):
+    def check_hours_filled(self):
         for rec in self:
-            if rec.session_id:
-                rec.trainer_id = rec.session_id.trainer_id
-                rec.location_id = rec.session_id.location_id
+            if not rec.end_hour or not rec.start_hour:
+                raise exceptions.UserError(_("Start and end hours are required"))
 
     def write(self, vals):
         result = super(TrainingSitting, self).write(vals)
         self.check_simultaneous_sittings()
+        self.check_hours_filled()
         return result
 
     @api.model
     def create(self, vals):
         result = super(TrainingSitting, self).create(vals)
         result.check_simultaneous_sittings()
+        result.check_hours_filled()
         return result
 
 
