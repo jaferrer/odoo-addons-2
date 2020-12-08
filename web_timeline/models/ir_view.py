ed20dca79a04c7d24299918a3d6518f230294f53
Alexis Pasquier <alexis.pasquier@ndp-systemes.fr>
2020-12-08 18:43:57 +0100

    T208 - Planning OA => esprit Plan de charge NDP - odoo-addons/ouestacro#197

---
+++ web_timeline/models/ir_view.py
@@ -0,0 +1,19 @@
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+TIMELINE_VIEW = ('timeline', 'Timeline')
+
+
+class IrUIView(models.Model):
+    _inherit = 'ir.ui.view'
+
+    type = fields.Selection(selection_add=[TIMELINE_VIEW])
+
+
+class IrActWindowView(models.Model):
+    _inherit = 'ir.actions.act_window.view'
+
+    view_mode = fields.Selection(selection_add=[TIMELINE_VIEW])
